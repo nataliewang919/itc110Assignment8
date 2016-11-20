b4526103bcd4008408b3f973f680a6b0e2728767
nataliewang919 <hwang031@seattlecentral.edu>
2016-11-20 02:02:20 -0800

    Add files via upload

---
+++ Bill.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment8
+{
+    public class Bill
+    {
+        const double tax = 0.09;
+        private double tips;
+        private double amount;       
+        
+        public Bill()
+        {
+            TipPercent = 0;
+                Amount = 0;          
+        }
+     
+        public double TipPercent { set; get; }
+        public double Amount { set; get; }        
+        
+
+        public double GetTips(double TipPercent)
+        {           
+            return Amount * (TipPercent/100);         
+        }
+
+        public double GetTax()
+        {
+            return Amount * tax;
+        }
+
+        public double GetTotalAmount(double TipPercent)
+       {          
+            return Amount+GetTax() + GetTips(TipPercent);            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Assignment8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonSumbit_Click(object sender, RoutedEventArgs e)
+        {
+            Bill b = new Assignment8.Bill();          
+            b.Amount = double.Parse(textBoxMeal.Text);
+            
+            double tip;
+            if (radioButtonTen.IsChecked == true)
+            {
+                tip = 10;
+            }
+            else if (radioButtonFifteen.IsChecked == true)
+            {
+                tip = 15;
+            }
+            else if (radioButtonTwenty.IsChecked == true)
+            {
+                tip = 20;
+            }
+            else
+            {
+                tip = double.Parse(textBoxOther.Text);
+            }
+
+            labelResult.Content = "The total tax is " + b.GetTax().ToString() + " Your tip is " + b.GetTips(tip).ToString() + " Your total amount is " + b.GetTotalAmount(tip);
+        }
+    }
+}
